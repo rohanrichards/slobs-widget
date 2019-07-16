90ceaeaceb107e12d67aba366dcd6673cb71b811
Rohan Richards <rohan.m.richards@gmail.com>
2019-07-16 12:01:20 +1000

    include rxjs
    added rxjs and a simple timer module to wrap timer from rxjs

--- app/modules/timers.js
+++ app/modules/timers.js
@@ -1,6 +1,17 @@
+import { timer } from 'rxjs'
+
+// currently just a simple wrapper for rxjs timers
 class Timers {
 	constructor() {
 		console.log("Timers initializing")
+	}
+
+	once(time) {
+		return timer(time)
+	}
+
+	periodic(time) {
+		return timer(time, time)
 	}
 }
 
